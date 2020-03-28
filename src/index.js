e826d26588336884054093f00cfea145a414171a
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2020-03-28 14:51:34 -0300

    Rearrange components folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Likert from './components/items/Likert/Likert';
+import Likert from './components/Likert/Likert';
 import './index.css';
 
 ReactDOM.render(<Likert statement = "Item statement" numberOfChoices = "7" />, document.getElementById('root'));

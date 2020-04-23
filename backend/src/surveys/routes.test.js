a575caa29a985ba54366951125c31d237c54123a
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2020-04-23 07:50:16 -0300

    Shut down the server after tests run

--- backend/src/surveys/routes.test.js
+++ backend/src/surveys/routes.test.js
@@ -5,8 +5,18 @@ import supertest from 'supertest';
 
 jest.mock('./surveyModel');
 
+let app;
+
 describe('Survey controller', () => {
-  const app = express().use(surveys);
+  beforeAll(() => {
+    app = express()
+      .use(surveys)
+      .listen(3004);
+  });
+
+  afterAll(() => {
+    app.close();
+  });
 
   it('serves survey information', () => {
     let query = {

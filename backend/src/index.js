6bde4b80de3336e62bc7fa7279c777b184174403
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2020-04-25 04:08:55 -0300

    Connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import surveys from './surveys/surveyRoutes.js';
 
 const app = express();
 const port = process.env.BACKEND_PORT || 3004;
 
+const user = process.env.DATABASE_USERNAME || 'survey';
+const pass = process.env.DATABASE_PASSWORD || 'survey';
+
 app.use(surveys);
 
-export default app.listen(port, () => { 
+mongoose.connect(`mongodb://${user}:${pass}@localhost:27017/survey`, {
+  useNewUrlParser: true
+});
+
+app.listen(port, () => {
   console.log(`Survey backend listening at http://localhost:${port}`); 
 });

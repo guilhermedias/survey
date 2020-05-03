f6e5652185d8fa32f27c39d2ffbb3a4472284694
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2020-05-03 03:08:29 -0300

    Add skeleton code for saving surveys

--- backend/src/surveys/surveyRoutes.js
+++ backend/src/surveys/surveyRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 export default (surveyModel) => {
   const routes = express();
+
+  routes.use(bodyParser.json());
 
   routes.get('/', async (request, response) => {
     let surveys = await surveyModel.find().exec();
@@ -13,6 +16,11 @@ export default (surveyModel) => {
     let survey = await surveyModel.findById(id).exec();
 
     response.send(survey);
+  });
+
+  routes.post('/', async (request, response) => {
+    console.log(request.body);
+    response.sendStatus(201);
   });
 
   return routes;

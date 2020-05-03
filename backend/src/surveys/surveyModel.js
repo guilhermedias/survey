64854fe94f934410b6e84be0332c9a7e6b2a662c
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2020-05-03 02:35:30 -0300

    Adapt survey model to data structure

--- backend/src/surveys/surveyModel.js
+++ backend/src/surveys/surveyModel.js
@@ -2,9 +2,16 @@ import mongoose from 'mongoose';
 
 
 export default (autoIncrementPlugin) => {
+  let itemSchema = new mongoose.Schema({
+    id: Number,
+    statement: String
+  });
+
   let surveySchema = new mongoose.Schema({
     _id: Number,
-    description: String
+    description: String,
+    numberOfChoices: Number,
+    items: [ itemSchema ]
   }, {
     _id: false
   });

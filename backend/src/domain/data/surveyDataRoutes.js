ba769fc1a27b2bc68d860cb164dd67961a6d1220
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2020-05-20 22:27:19 -0300

    Refactor survey data controller

--- backend/src/domain/data/surveyDataRoutes.js
+++ backend/src/domain/data/surveyDataRoutes.js
@@ -3,6 +3,16 @@ import express from 'express';
 export default (surveyDataModel) => {
   const routes = express();
 
+  routes.post('/', async (request, response) => {
+    let newSurveyData = request.body;
+
+    let createdSurveyData = await surveyDataModel.create(newSurveyData);
+
+    response
+      .status(201)
+      .send(createdSurveyData);
+  });
+
   routes.get('/', async (request, response) => {
     let filter = {};
 
@@ -16,25 +26,21 @@ export default (surveyDataModel) => {
   });
 
   routes.get('/:id', async (request, response) => {
-    let surveyData = await surveyDataModel.findOne({
+    let query = {
       surveyDataId: request.params.id
-    }).exec();
+    };
+
+    let surveyData = await surveyDataModel.findOne(query).exec();
 
     response.send(surveyData);
   });
 
-  routes.post('/', async (request, response) => {
-    let createdSurveyData = await surveyDataModel.create(request.body);
-
-    response
-      .status(201)
-      .send(createdSurveyData);
-  });
-
   routes.delete('/:id', async (request, response) => {
-    await surveyDataModel.deleteOne({
+    let query = {
       surveyDataId: request.params.id
-    }).exec();
+    };
+
+    await surveyDataModel.deleteOne(query).exec();
 
     response
       .status(204)

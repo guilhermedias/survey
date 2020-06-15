e7556ac2bbd35f6f2cbb50dc956f37ad9de3a0f1
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2020-06-15 19:50:10 -0300

    Use helper methods in the survey validator test suite

--- backend/test/unit/domain/surveys/surveyValidators.test.js
+++ backend/test/unit/domain/surveys/surveyValidators.test.js
@@ -2,138 +2,111 @@ import surveyValidators from '../../../../src/domain/surveys/surveyValidators.js
 
 describe('The survey validation module', () => {
   it('validates the description in the create survey request body', () => {
-    let request = {
-      originalUrl: '/surveys',
-      method: 'POST',
-      body: {
-        'numberOfChoices': 5,
-        'items': [
-          {
-            'id': 1,
-            'statement': 'Statement 1.'
-          }
-        ]
-      }
-    };
+    let request = buildCreateSurveyRequest({
+      'numberOfChoices': 5,
+      'items': [
+        {
+          'id': 1,
+          'statement': 'Statement 1.'
+        }
+      ]
+    });
 
     let errors = applyMatchingValidatorsTo(request);
 
-    expect(errors).toHaveLength(1);
-    expect(errors).toEqual(expect.arrayContaining([
-      {
-        path: 'description',
-        message: 'Survey description is required.'
-      }
-    ]));
+    assertThatErrorsContainsExactly(errors, {
+      path: 'description',
+      message: 'Survey description is required.'
+    });
   });
 
   it('validates the number of choices in the create survey request body', () => {
-    let request = {
-      originalUrl: '/surveys',
-      method: 'POST',
-      body: {
-        'description': 'Survey 1.',
-        'items': [
-          {
-            'id': 1,
-            'statement': 'Statement 1.'
-          }
-        ]
-      }
-    };
+    let request = buildCreateSurveyRequest({
+      'description': 'Survey 1.',
+      'items': [
+        {
+          'id': 1,
+          'statement': 'Statement 1.'
+        }
+      ]
+    });
 
     let errors = applyMatchingValidatorsTo(request);
 
-    expect(errors).toHaveLength(1);
-    expect(errors).toEqual(expect.arrayContaining([
-      {
-        path: 'numberOfChoices',
-        message: 'Number of choices is required.'
-      }
-    ]));
+    assertThatErrorsContainsExactly(errors, {
+      path: 'numberOfChoices',
+      message: 'Number of choices is required.'
+    });
   });
 
   it('validates the items in the create survey request body', () => {
-    let request = {
-      originalUrl: '/surveys',
-      method: 'POST',
-      body: {
-        'description': 'Survey 1.',
-        'numberOfChoices': 5
-      }
-    };
+    let request = buildCreateSurveyRequest({
+      'description': 'Survey 1.',
+      'numberOfChoices': 5
+    });
 
     let errors = applyMatchingValidatorsTo(request);
 
-    expect(errors).toHaveLength(1);
-    expect(errors).toEqual(expect.arrayContaining([
-      {
-        path: 'items',
-        message: 'Survey items are required.'
-      }
-    ]));
+    assertThatErrorsContainsExactly(errors, {
+      path: 'items',
+      message: 'Survey items are required.'
+    });
   });
 
   it('validates the item ID in the create survey request body', () => {
-    let request = {
-      originalUrl: '/surveys',
-      method: 'POST',
-      body: {
-        'description': 'Survey 1.',
-        'numberOfChoices': 5,
-        'items': [
-          {
-            'statement': 'Statement 1.'
-          },
-          {
-            'id': 2,
-            'statement': 'Statement 2.'
-          }
-        ]
-      }
-    };
+    let request = buildCreateSurveyRequest({
+      'description': 'Survey 1.',
+      'numberOfChoices': 5,
+      'items': [
+        {
+          'statement': 'Statement 1.'
+        },
+        {
+          'id': 2,
+          'statement': 'Statement 2.'
+        }
+      ]
+    });
 
     let errors = applyMatchingValidatorsTo(request);
 
-    expect(errors).toHaveLength(1);
-    expect(errors).toEqual(expect.arrayContaining([
-      {
-        path: 'items[0].id',
-        message: 'Survey item ID is required.'
-      }
-    ]));
+    assertThatErrorsContainsExactly(errors, {
+      path: 'items[0].id',
+      message: 'Survey item ID is required.'
+    });
   });
 
   it('validates the item statement in the create survey request body', () => {
-    let request = {
-      originalUrl: '/surveys',
-      method: 'POST',
-      body: {
-        'description': 'Survey 1.',
-        'numberOfChoices': 5,
-        'items': [
-          {
-            'id': 1,
-            'statement': 'Statement 1.'
-          },
-          {
-            'id': 2,
-          }
-        ]
-      }
-    };
+    let request = buildCreateSurveyRequest({
+      'description': 'Survey 1.',
+      'numberOfChoices': 5,
+      'items': [
+        {
+          'id': 1,
+          'statement': 'Statement 1.'
+        },
+        {
+          'id': 2,
+        }
+      ]
+    });
 
     let errors = applyMatchingValidatorsTo(request);
 
-    expect(errors).toHaveLength(1);
-    expect(errors).toEqual(expect.arrayContaining([
-      {
-        path: 'items[1].statement',
-        message: 'Survey item statement is required.'
-      }
-    ]));
+    assertThatErrorsContainsExactly(errors, {
+      path: 'items[1].statement',
+      message: 'Survey item statement is required.'
+    });
   });
 });
+
+function buildCreateSurveyRequest(requestBody) {
+  return {
+    originalUrl: '/surveys',
+    method: 'POST',
+    body: requestBody
+  };
+}
 
 function applyMatchingValidatorsTo(request) {
   let errors = [];
@@ -145,4 +118,9 @@ function applyMatchingValidatorsTo(request) {
   });
 
   return errors;
+}
+
+function assertThatErrorsContainsExactly(actualErrors, expectedError) {
+  expect(actualErrors).toHaveLength(1);
+  expect(actualErrors).toEqual(expect.arrayContaining([ expectedError ]));
 }
